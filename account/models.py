efe4db89e85801cdc756ff184f0f39e8fb567b91
isri21 <israelalemu21@gmail.com>
2025-01-01 11:51:39 +0300

    Edit Rating & Review Model Fields

--- account/models.py
+++ account/models.py
@@ -11,7 +11,8 @@ class Review(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="reviews")
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="reviewed")
 	review = models.TextField()
-	review_date = models.DateField(auto_now_add=True)
+	review_date = models.DateTimeField(auto_now_add=True)
+	edited_at = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		constraints = [
@@ -26,7 +27,8 @@ class Rating(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="ratings")
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="rated")
 	rating = models.DecimalField(max_digits=2, decimal_places=1)
-	rating_date = models.DateField(auto_now_add=True)
+	rating_date = models.DateTimeField(auto_now_add=True)
+	edited_at = models.DateTimeField(auto_now=True)
 
 	class Meta:
 		constraints = [
