94f7161bc98beaefb680cb838196c48a3ac69d77
isri21 <israelalemu21@gmail.com>
2025-01-09 14:32:56 +0300

    Modify Comment for Serializers and Views for Account App

--- account/views.py
+++ account/views.py
@@ -1,67 +1,64 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import get_user_model
 from .serializers import *
 from .permissions import *
 from rest_framework.permissions import IsAuthenticated
-from store.serializers import DetailCategorySerializer, ReviewSerializer, RatingSerializer, GeneralProductsSerializer, CreateProductSerialzier, ViewDetailProdcutSerializer
+from store.serializers import *
 from store.functions import BasicPagination
 from store.models import Product, Category, Product_Category, Purchase
 from .models import Wishlist, Review, Rating
 
-User = get_user_model()
-
-
-# View for viewing and updating user profile
+# Handle viewing and updating user profile
 @api_view(["GET", "PATCH"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def profile_info(request):
 	# Get the user
 	user = request.user
 
-	# Logic for GET method
+	# Logic for viewing profile detail
 	if request.method == "GET":
 		serialized = ProfileSerializer(user) # Instantiate serializer with user data
 		return Response(serialized.data, status=status.HTTP_200_OK) # Return serialized data
 	
-	# Logic for PATCH method
+	# Logic for updating profile details
 	if request.method == "PATCH":
-		# Check if request data is empty
+
+		# Ensure request body is not empty
 		if not request.data:
 			return Response({
 				"field": "No update fields were specified."
 			}, status=status.HTTP_400_BAD_REQUEST)
 
 		# Instantiate serializer, while binding it to the user object and allowing partial fields
+		# Pass only_profile context so that it only returns profile details, and not finance detials, and other data
 		serialized = ProfileSerializer(instance=user, data=request.data, partial=True, context={"only_profile": True})
-		if serialized.is_valid(raise_exception=True): # Check if the data sent is valid
-			# If data is valid, save it and return the serialized data, along with a success message
+		
+		# Check if the input data is valid, and respond accordingly
+		if serialized.is_valid(raise_exception=True):
 			serialized.save()
 			return Response({
 				"status": f"Update succesfull.",
 				"updated_profile": serialized.data
 			}, status=status.HTTP_200_OK)
 		
-
-# View for viewing and creating categoies
+# Handle viewing categoires created, and updating categoires
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_create_categories(request):
 	# Get user
 	user = request.user
 
-	# Logic for GET method
+	# Logic for getting categories created
 	if request.method == "GET":
-		# instantiate the custom paginator
+		# Prepare paginator
 		paginator = BasicPagination()
 
-		# fetch the products the user created using prefetch_related for optimization, and only show product that are not marked deleted
+		# Get all the categories created by the user
 		categories = Category.objects.filter(creator=user, is_deleted=False)
 
-		# Check if the product queryset is not empty
+		# Check if the product queryset is not empty, if it is respond accordingly
 		if not categories.exists():
-			# If it is empty, return a 404 NOT FOUND
 			return Response({
 				"no_categories": "You have not created any products yet."
 			}, status=status.HTTP_204_NO_CONTENT)
@@ -73,12 +70,13 @@ def list_create_categories(request):
 		# Reutrn the serialized quieryset
 		return paginator.get_paginated_response(serializer.data)
 
-	# Logic for POST method
+	# Logic for creating a product
 	if request.method == "POST":
+		# Get the user
 		user = request.user
-		# Instantiate an IsAuthenticated Instance
+		# Prepare IsAuthenticated class
 		permission = IsAuthenticated()
-		# Check if user doesn't have permission, if not return error
+		# Check if user is authenticated
 		if not permission.has_permission(request, None):
 			return Response({
 				"detail": "Authentication credentials were not provided."
@@ -86,7 +84,7 @@ def list_create_categories(request):
 
 		# Deserialize incoming data
 		serializer = DetailCategorySerializer(data=request.data)
-		# Check if incoming data is valid, if it save and return it, else return error (raise_exception=True)
+		# Check if incoming data is valid, and respond accordingly
 		if serializer.is_valid(raise_exception=True):
 			serializer.save(user=user)
 			return Response({
@@ -94,8 +92,9 @@ def list_create_categories(request):
 				"category": serializer.data
 			}, status=status.HTTP_200_OK)
 
-
-# View for updating and deleting categoies
+# Handle updating and deleting categories
+# -> Used PUT instead of PATCH b/c the only editable thing of a category is the name -
+# 	 So if we update it, by default it is a full update and not a partial one.
 @api_view(["PUT", "DELETE"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def manage_categories(request, id):
@@ -107,7 +106,7 @@ def manage_categories(request, id):
 		return Response({"error": "Category does not exist."}, status=status.HTTP_404_NOT_FOUND)
 	
 
-	# Check if user doesn't have permission, it doesn't return error
+	# Check if user doesn't have permission, it user doesn't return error
 	permission = IsCategoryOwner()
 	if not permission.has_object_permission(request, category):
 		return Response({
@@ -115,15 +114,18 @@ def manage_categories(request, id):
 		}, status=status.HTTP_401_UNAUTHORIZED)
 	
 		
-	# Check if there are any associated products with a category
+	# Check if there are any associated products with a category, if there are return error
 	if Product_Category.objects.filter(category=category).exists():
 		return Response({
 			"forbidden": "There are products associated with this category, there for you can not edit it."
 		}, status=status.HTTP_403_FORBIDDEN)
 	
-	# Logic for PUT method
+	# Logic updating category
 	if request.method == "PUT":
+		# Pass incomming data (category name) to serialzier, while binding it to the instance
 		serializer = DetailCategorySerializer(instance=category, data=request.data)
+
+		# Check if the incoming data is valid, and respond accordingly
 		if serializer.is_valid(raise_exception=True):
 			serializer.save()
 			return Response({
@@ -131,34 +133,32 @@ def manage_categories(request, id):
 				"updated": serializer.data
 			}, status=status.HTTP_200_OK)
 		
-	# Logic for DELETE method
+	# Logic for deleting a category
 	if request.method == "DELETE":
+		# Set the is_deleted field of to True
 		category.is_deleted = True
 		category.save()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
-
-# View for viewing wishlist item of authenticated user
+# Handle getting products from user wishlist
 @api_view(["GET"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_wishlist_items(request):
-	# get user object
+	# Get user object
 	user = request.user
 
-	# Query wishlist based on user
+	# Query wishlist based on user and check if it is empty, if it is return error
 	wishlist = Wishlist.objects.filter(user=user)
-	
-	# If queryset empty return error
 	if not wishlist.exists():
 		return Response({
 			"no_products": "You have no products in you wishlist."
 		}, status=status.HTTP_204_NO_CONTENT)
 	
-	# If not empty reutrn data
+	# Serialize queryset and return 
 	serializer = WishListSerializer(wishlist, many=True)
 	return Response(serializer.data, status=status.HTTP_200_OK)
 
-# View for deleting wish list item of authenticated user
+# Handle deletion of products from users wish list
 @api_view(["DELETE"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def delete_wishlist_item(request, id):
@@ -177,19 +177,18 @@ def delete_wishlist_item(request, id):
 	item.delete()
 	return Response(status=status.HTTP_204_NO_CONTENT)
 
+# Handle getting the purchased items of a user
 @api_view(["GET"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_purchases(request):
 	# Get the user
 	user = request.user
 
-	# Instantiate paginator
+	# Prepare paginator
 	paginator = BasicPagination()
 
-	# Query purchase based on user
+	# Query purchase based on user, and check if empty, respond accordingly
 	purchases = Purchase.objects.filter(user=user).order_by("-purchase_date")
-
-	# Check if empty and return status
 	if not purchases.exists():
 		return Response({
 			"no_items": "You have no purchased products."
@@ -197,22 +196,22 @@ def list_purchases(request):
 
 	# Paginate data
 	paginated = paginator.paginate_queryset(purchases, request)
+	
 	# Serialized the paginated data
 	serializer = ProfilePurchasesSerialzier(paginated, many=True)
 
 	# Return paginated data
 	return paginator.get_paginated_response(serializer.data)
 
+# Handle getting the reviews of a user
 @api_view(["GET"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_reviews(request):
 	# Get the user
 	user = request.user
 
-	# Query reviews based on user
+	# Query reviews based on user, and check if it is empty, if it is respond accrdingly
 	reviews = Review.objects.filter(user=user)
-
-	# Check if queryset is empty
 	if not reviews.exists():
 		return Response({
 			"no_reviews": "You have not reviewd any products yet."
@@ -224,10 +223,12 @@ def list_reviews(request):
 
 	# Serialize the paginated queryset
 	serializer = ReviewSerializer(paginated, many=True, context={"remove_user": True})
-	# return the data
+	
+	# Return the data
 	return paginator.get_paginated_response(serializer.data)
 
-@api_view(["PATCH", "DELETE"])
+# Handle updating and deleting user reviews for a product
+@api_view(["PUT", "DELETE"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def manage_reviews(request, id):
 	# Try to get the review, if not exist return error
@@ -261,17 +262,16 @@ def manage_reviews(request, id):
 	if request.method == "DELETE":
 		review.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
-	
+
+# Handle getting the products that a user rated
 @api_view(["GET"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_ratings(request):
 	# Get the user
 	user = request.user
 
-	# Query ratings based on user
+	# Query ratings based on user, and check if empty, if it is respond accordingly
 	ratings = Rating.objects.filter(user=user)
-
-	# Check if queryset is empty
 	if not ratings.exists():
 		return Response({
 			"no_ratings": "You have not rated any products yet."
@@ -286,7 +286,8 @@ def list_ratings(request):
 	# return the data
 	return paginator.get_paginated_response(serializer.data)
 
-@api_view(["PATCH", "DELETE"])
+# Handle updating and deleting user ratings for a product
+@api_view(["PUT", "DELETE"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def manage_ratings(request, id):
 	# Try to get the ratings, if not exist return error
@@ -316,22 +317,22 @@ def manage_ratings(request, id):
 				"updated_rating": serializer.data
 			}, status=status.HTTP_200_OK)
 		
-	# Logic of deleting a reiview
+	# Logic of deleting a review
 	if request.method == "DELETE":
 		rating.delete()
 		return Response(status=status.HTTP_204_NO_CONTENT)
 
+# Handle viewing products created, and updating products
 @api_view(["GET", "POST"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view
 def list_create_products(request):
-	# get the user
+	# Get the user
 	user = request.user
-	if request.method == "GET":
-	
-		# Query the model based on the user
-		products = Product.objects.filter(owner=user, is_deleted=False)
 
-		# Check if the Query is empty, if it is return an error
+	# Logic for getting products created
+	if request.method == "GET":
+		# Get products created by the user and ones that are not deleted, and check if not empty, if empty respond accordingly
+		products = Product.objects.filter(owner=user, is_deleted=False)
 		if not products.exists():
 			return Response({
 				"no_products": "You have not created any products yet."
@@ -341,23 +342,28 @@ def list_create_products(request):
 		paginator = BasicPagination()
 		paginated = paginator.paginate_queryset(products, request)
 
-		# If it is not empty serialize and return it
+		# Serialize queryset and return with meta-data
 		serializer = GeneralProductsSerializer(paginated, many=True)
 		return paginator.get_paginated_response(serializer.data)
 	
+	# Logic for creating a product
 	if request.method == "POST":
-		# print(request.data)
+		# Deserialze incoming data
 		serializer = CreateProductSerialzier(data=request.data)
+
+		# Check if serializer is valid
 		if serializer.is_valid(raise_exception=True):
 			serializer.save(owner=user) # Save the product if valid
 			id = serializer.data["id"] # Get the prodcut id of saved product
 			new_product = Product.objects.get(id=id) # Get the saved prodcut form the model
-			new_serializer = ViewDetailProdcutSerializer(new_product, context={"only_product": True}) # Serialize the new product
+			# Serialize the new product with other serialzier to return in success message
+			new_serializer = ViewDetailProdcutSerializer(new_product, context={"only_product": True})
 			return Response({
 				"status": "Successfully Created a New Product",
 				"product": new_serializer.data # Return the new product details
 			}, status=status.HTTP_201_CREATED)
 
+# Handle update and deletion of a product
 @api_view(["PATCH", "DELETE"])
 @permission_classes([IsAuthenticated]) # Only authenticated users can access view	
 def manage_products(request, id):
@@ -390,15 +396,16 @@ def manage_products(request, id):
 
 		# Serialize incoming data while bining it to the product and enabling partial update
 		serializer = CreateProductSerialzier(instance=product, data=request.data, partial=True)
+
 		# Check incoming data validity, if not return error
 		if serializer.is_valid(raise_exception=True):
 			serializer.save(owner=user) # Save the product if valid
-			id = serializer.data["id"] # Get the prodcut id of saved product
-			new_product = Product.objects.get(id=id) # Get the saved prodcut form the model
-			new_serializer = ViewDetailProdcutSerializer(new_product, context={"only_product": True}) # Serialize the new product
+
+			# Serialize the new product with other serialzier to return in success message
+			deatail_serialize = ViewDetailProdcutSerializer(product, context={"only_product": True})
 			return Response({
 				"status": "Successfully Updated the Product",
-				"product": new_serializer.data # Return the new product details
+				"product": deatail_serialize.data
 			}, status=status.HTTP_200_OK)
 		
 	# Logic for deleting a product
