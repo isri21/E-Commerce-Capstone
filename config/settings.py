cc458e07d52933655c687a707dd96e61446160f1
isri21 <israelalemu21@gmail.com>
2025-07-28 11:48:13 +0300

    Hard Code Secret Key

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "django-insecure-kv%43e2nc4i-&t-0se91x#s+fv=^@tm*0*36drq6(d0dcsp"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
